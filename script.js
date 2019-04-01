d8e58e0230e7cb9be9b2abf471f69f4c7e9c5f64
Muhammad Irfan <40903256+MDotIrfan@users.noreply.github.com>
2019-04-01 16:01:50 +0700

    update input number only

--- script.js
+++ script.js
@@ -94,3 +94,9 @@ function hasil(){
 	document.querySelector('#input').value = eval(hasil);
 	console.log(hasilakhir);
 }
+function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
